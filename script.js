20dbfca5d55c698d28249511dadcd1ed02c9f7f9
theoeyerov <thiyophin.j@eyerov.com>
2023-10-02 08:17:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("nav-learn").addEventListener("mouseover", function () {
+  document.querySelector(".nav-learn-content").style.display = "flex";
+});
+document.getElementById("nav-learn").addEventListener("mouseout", function () {
+  document.querySelector(".nav-learn-content").style.display = "none";
+});
+
+document.getElementById("nav-teach").addEventListener("mouseover", function () {
+  document.querySelector(".nav-teach-content").style.display = "flex";
+});
+document.getElementById("nav-teach").addEventListener("mouseout", function () {
+  document.querySelector(".nav-teach-content").style.display = "none";
+});
+
+document.getElementById("nav-research").addEventListener("mouseover", function () {
+  document.querySelector(".nav-research-content").style.display = "flex";
+});
+document.getElementById("nav-research").addEventListener("mouseout", function () {
+  document.querySelector(".nav-research-content").style.display = "none";
+});
+
+document.getElementById("nav-computers").addEventListener("mouseover", function () {
+  document.querySelector(".nav-computers-content").style.display = "flex";
+});
+document.getElementById("nav-computers").addEventListener("mouseout", function () {
+  document.querySelector(".nav-computers-content").style.display = "none";
+});
+
+document.getElementById("nav-aboutus").addEventListener("mouseover", function () {
+  document.querySelector(".nav-aboutus-content").style.display = "flex";
+});
+document.getElementById("nav-aboutus").addEventListener("mouseout", function () {
+  document.querySelector(".nav-aboutus-content").style.display = "none";
+});
